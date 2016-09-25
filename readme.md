2e72c5524788cabb5fd9a458ef827e10e7e36dc5
Darell Koh <darellkoh@gmail.com>
2016-09-25 09:43:23 -0400

    init

--- readme.md
+++
@@ -1,3 +0,0 @@
-# A 2D-game using Phaser.io
-
-Built for Fullstack Academy's Stackathon project.
--- server.js
+++ server.js
@@ -1,19 +1,7 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
-var morgan = require('morgan');
 
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded( { extended: true }));
-
-app.use(morgan('dev'));
-app.use(express.static(__dirname + '/js'));
 app.use(express.static(__dirname + '/styles'));
-
-
-
-
 
 
 // Listening
