fa5ead565a46bb96063fa4b1fdbb28c52164595f
Darell Koh <darellkoh@gmail.com>
2016-09-26 10:49:15 -0400

    changed port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.use(express.static(__dirname + '/styles'));
 
 
 // Listening
-var port = 3000;
+var port = process.env.PORT || 3000;
 app.listen(port, function(){
   console.log('Listening on port ' + port);
 });
